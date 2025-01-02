fd2daf0d47b8475c5785a6c5d780fe0a6863e8e8
John Rellis <john.d.rellis@gmail.com>
2025-01-02 00:06:41 +0000

    updated presentation

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let first = 22;
+    let second = 33;
+
+    let sum = add(first, second);
+
+    println!("the sum of of {first} and {second} are {sum}");
+
+    // a string could be retrieved from input
+    // so undefined size at compile time
+    let s1: String = String::from("hello");
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
 }
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,15 @@
+// fn main() {
+//     let mut x = 5; // HERE!
+//     println!("The value of x is : {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
 fn main() {
-    let x = 5;
-    println!("The value of x is : {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
 }
